da71801aba160cbd4956d90820c28098cb32ddb6
Samuel <samuel.caillaud@epsi.fr>
2018-11-29 11:49:14 +0100

    First Commit

---
+++ AvionBinder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ProjetAirAtlantique
+{
+    class AvionBinder : INotifyPropertyChanged
+    {
+        private int disponibilité;
+
+        public AvionBinder()
+        {
+
+        }
+
+        public AvionBinder()
+    }
+}
---
+++ ConnexionMySQL.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace ProjetAirAtlantique
+{
+    class ConnexionMySQL
+    {
+        private MySqlConnection connexion;
+        private string server;
+        private string database;
+        private string user;
+        private string password;
+
+
+        public Boolean OpenConnection()
+        {
+            server = "localhost";
+            database = "dba_air_atlantique";
+            user = "dba_air_atlantique";
+            password = "soosabharrt2ld";
+            string connectionString = "SERVER=" + server + ";" +
+                                      "DATABASE=" + database + ";" +
+                                      "UID=" + user + ";" +
+                                      "PASSWORD=" + password + ";";
+
+            connexion = new MySqlConnection(connectionString);
+
+            connexion.Open();
+
+            return true;
+        }
+
+        public List<AvionBinder> SelectAvions()
+        {
+            List<AvionBinder> ListAvions = new List<AvionBinder>();
+
+            string query = "SELECT * FROM avions;";
+            MySqlCommand cmd = new MySqlCommand(query, connexion);
+            cmd.ExecuteNonQuery();
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                AvionBinder a = new AvionBinder(reader.GetString(0), reader.GetString(1));
+                ListAvions.Add(a);
+            }
+            return ListAvions;
+        }
+    }
+}
